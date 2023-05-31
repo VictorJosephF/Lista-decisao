394f1610852462bee51f165ea71915c91ff79ad0
VICTOR JOSEPH FARIA <68386487+VictorJosephF@users.noreply.github.com>
2023-05-31 16:14:05 -0300

    Update Q25.java

--- src/listaDescisao/Q25.java
+++ src/listaDescisao/Q25.java
@@ -1,2 +1,52 @@
-package listaDescisao;public class Q25 {
+package listaDescisao;
+import java.util.Scanner;
+public class Q25 {
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        int validade = 0;
+        String resposta;
+        System.out.println("Responda as seguintes perguntas com Sim ou Não!");
+        System.out.println("telefonou para a vítimas?");
+        resposta = read.next();
+
+        if(resposta.equals("Sim")){
+            validade += 1;
+        }
+        System.out.println("Esteve no local do crime?");
+        resposta = read.next();
+
+         if(resposta.equals("Sim")){
+            validade +=1;
+        }
+        System.out.println("Mora perto do crime?");
+        resposta = read.next();
+
+        if(resposta.equals("Sim")){
+            validade +=1;
+        }
+        System.out.println("Devia para a vítima?");
+        resposta = read.next();
+
+        if(resposta.equals("Sim")){
+            validade +=1;
+        }
+        System.out.println("Já trabalhou para a vítima?");
+        resposta = read.next();
+
+        if(resposta.equals("Sim")){
+            validade +=1;
+        }
+        if(validade == 2){
+            System.out.println("Classificação: Suspeita!");
+        }
+        if(validade >=3 &&  validade <= 4){
+            System.out.println("Classificação: Cúmplice!");
+        }
+        if(validade == 5){
+            System.out.println("Classificação: Culpado!");
+        }
+        if(validade < 2){
+            System.out.println("Classificação: Inocente!");
+        }
+    }
 }
